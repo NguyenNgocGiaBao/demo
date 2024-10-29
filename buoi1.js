ec474b3ee339b93a6bea3cda232a7f864a0fdf4a
NguyenNgocGiaBao <baonngps40454@gmail.com>
2024-10-29 17:41:39 +0700

    demo

---
+++ buoi1.js
@@ -0,0 +1,49 @@
+// let name_ = 'ABC',bao="bao"
+// console.log('Tên này là:', name_,bao)
+// console.log(`Tên này là ${name_} ${bao}`)
+
+// function count(count){
+//     for(let i = 0; i < count; i++ )
+//         console.log(`số : ${i}`);
+
+// }
+
+// count(10)
+
+
+// let my_arr =[`a`,`b`,`c`]
+
+// function printName(name){
+//     for(let i = 0; i < my_arr.length; i++)
+//         console.log(`${my_arr[i]}`);
+// }
+
+// printName(my_arr);
+
+let my_arr =[`a`,`b`,`c`]
+
+// function printName(my_arr){
+//     for(let i in my_arr)
+//         console.log(`${my_arr[i]}`);
+// }
+
+// printName(my_arr);
+
+// function printName(my_arr){
+//     for(let i of my_arr)
+//         console.log(i);
+// }
+
+// printName(my_arr);
+
+function printName(my_arr){
+    forEach(function(){
+
+    })
+        console.log();
+}
+
+printName(my_arr);
+
+
+
